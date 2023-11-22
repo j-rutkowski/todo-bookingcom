9d44d82295c98703261f7f126d31bbbeac228148
Jakub Rutkowski <kuba.r0012@gmail.com>
2023-11-22 21:55:15 +0100

    disable drag on tasks

--- client/src/components/Task.tsx
+++ client/src/components/Task.tsx
@@ -46,7 +46,7 @@ function Task({ task, tasks, setTasks }: TaskProps) {
     }
 
     return (
-        <Reorder.Item value={task} className={`task ${task.completed ? 'checked' : ''}`}>
+        <Reorder.Item value={task} drag={false} className={`task ${task.completed ? 'checked' : ''}`}>
             <label className='task__label'>
                 <span>{task.title}</span>
                 <input
@@ -55,7 +55,8 @@ function Task({ task, tasks, setTasks }: TaskProps) {
                     checked={task.completed}
                     className='task__checkbox'
                 />
-                <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512" className="task__check">
+                <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"
+                     className="task__check">
                     <path fill="#FFFFFF" d="M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"/>
                 </svg>
             </label>
